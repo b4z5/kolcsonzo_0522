04af9f7c9a347610de993cb1ff010024f9c8526f
13asTerem <you@example.com>
2025-05-22 18:09:35 +0200

    customer done

--- VideoGame/Customer.cs
+++ VideoGame/Customer.cs
@@ -8,7 +8,37 @@ namespace VideoGame
 {
     public class Customer
     {
-     
+        public int CustomerID { get; set; }
+        public string Name { get; set; }
+        public List<Game> RentedGames { get; set; } = new List<Game>();
+
+        public void RentGame(Game game)
+        {
+            if (game.IsAvailable)
+            {
+                game.IsAvailable = false;
+                RentedGames.Add(game);
+            }
+        }
+
+        public void ReturnGame(Game game)
+        {
+            if (RentedGames.Contains(game))
+            {
+                game.IsAvailable = true;
+                RentedGames.Remove(game);
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} (ID: {CustomerID}) - Kikölcsönzött játékok: {RentedGames.Count}";
+        }
+
+        
     }
+
+
+
 
 }
